aa9b73e21e0807c765435b2ce4413ac0f8506c4c
wolandec <wolandec@gmail.com>
2018-02-08 21:46:57 +0300

    Settings activity started

--- app/src/main/java/wolandec/jeeves/SettingsActivity.kt
+++ app/src/main/java/wolandec/jeeves/SettingsActivity.kt
@@ -7,11 +7,19 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.Toast
 
 
-class SettingsActivity : AppCompatActivity() {
+
+
+class SettingsActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
+
     val LOG_TAG = this::class.java.simpleName
+    var listView: ListView? = null
 
     public override fun onStop() {
         super.onStop()
@@ -20,13 +28,33 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        listView = findViewById(R.id.listView)
         registerBroadcastService()
         checkDoNotDisturb()
+        fillList()
+        listView?.setOnItemClickListener(this)
+    }
+
+    override fun onItemClick(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        when (position){
+            0 ->{
+                startActivity(Intent(this, LocationActivity::class.java))
+            }
+            1 ->{
+                startActivity(Intent(this, CallActivity::class.java))
+            }
+        }
+    }
+
+    private fun fillList() {
+        val adapter = ArrayAdapter<String>(this,
+                R.layout.row_layout, R.id.label, resources.getStringArray(R.array.list_preferences))
+        listView?.setAdapter(adapter)
     }
 
     @SuppressLint("NewApi")
-    fun checkDoNotDisturb(){
-        if( Build.VERSION.SDK_INT < 23 ) {
+    private fun checkDoNotDisturb() {
+        if (Build.VERSION.SDK_INT < 23) {
             return;
         }
         val n = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
