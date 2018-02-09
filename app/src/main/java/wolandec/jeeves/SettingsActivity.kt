00c08db1655ca210ed0bc93a11ca04b80d7d3491
wolandec <wolandec@gmail.com>
2018-02-09 15:59:06 +0300

    Check permissions at start

--- app/src/main/java/wolandec/jeeves/SettingsActivity.kt
+++ app/src/main/java/wolandec/jeeves/SettingsActivity.kt
@@ -1,13 +1,17 @@
 package wolandec.jeeves
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 
@@ -24,6 +28,7 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkPermissions()
         val sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
         if (sharedPref?.getBoolean("enable_jeeves", false) == true) {
             registerBroadcastService()
@@ -37,6 +42,18 @@ class SettingsActivity : AppCompatActivity() {
         }
         sharedPref?.registerOnSharedPreferenceChangeListener(sharedPrefChangeListener);
 
+    }
+
+    fun checkPermissions(){
+
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.RECEIVE_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.RECEIVE_SMS,Manifest.permission.CALL_PHONE,Manifest.permission.ACCESS_COARSE_LOCATION),
+                        1)
+        }
         checkDoNotDisturb()
     }
 
